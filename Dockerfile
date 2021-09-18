b49eecfdf644c0e301404f8b79b4ee4cd1bd3c1d
Brett Lykins <brett.lykins@networktocode.com>
2021-09-18 14:09:44 -0400

    initial app structure

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+ARG PYTHON_VER
+
+FROM python:3.9-slim
+
+RUN pip install --upgrade pip \
+    && pip install poetry
+
+WORKDIR /local
+COPY pyproject.toml /local
+
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-interaction --no-ansi
---
+++ leanpub-multi-action/cli.py
@@ -0,0 +1,24 @@
+"""Leanpub Actions for GitHub Actions Workflows"""
+import sys
+
+import click
+
+
+@click.command()
+@click.option("--test", help="Test argument")
+def main(test: str) -> int:
+    """Entrypoint into our script.
+
+    Args:
+        test (str): testing and stuff
+
+    Returns:
+        int: exit_code as an integer to return to OS
+    """
+    exit_code = 0
+    print(test)
+    return exit_code
+
+if __name__ == "__main__":
+    exit_code = main()
+    sys.exit(exit_code)
