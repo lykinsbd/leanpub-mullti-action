d0dae906305f6931bb661e6c0b65f9b72f0b5e9f
Brett Lykins <lykinsbd@gmail.com>
2021-09-18 20:49:06 -0400

    initial leanpub client buildout

--- leanpub_multi_action/cli.py
+++ leanpub_multi_action/cli.py
@@ -1,22 +1,40 @@
 """Leanpub Actions for GitHub Actions Workflows."""
+
+import os
 import sys
 
 import click
 
 
 @click.command()
-@click.option("--test", help="Test argument")
-def main(test: str = None) -> int:
+@click.option("--leanpub_api_key", help="Leanpub API Key")
+def main(leanpub_api_key: str = None) -> int:
     """Entrypoint into our script.
 
     Args:
-        test (str): testing and stuff
+        leanpub_api_key (str): API Key for the Leanpub API.
+            If not provided, will attempt to gather from the
+            LEANPUB_API_KEY Environment Variable.
+            If neither are set, will error out.
 
     Returns:
         int: exit_code as an integer to return to OS
     """
     exit_code = 0
-    print(test)
+
+    # Attempt to find API Key
+    leanpub_api_key = leanpub_api_key or os.environ.get("LEANPUB_API_KEY", False)
+    if not leanpub_api_key:
+        exit_code = 1
+        print("No Leanpub API Key Found!")
+        return exit_code
+
+    err = None
+
+    if err is not None:
+        print(err)
+        exit_code = 1
+
     return exit_code
 
 
---
+++ leanpub_multi_action/leanpub.py
@@ -0,0 +1,42 @@
+"""Leanpub API Client and Helpers.
+
+API Docs: https://leanpub.com/help/api
+API URL: https://leanpub.com/
+"""
+
+from typing import Optional, Union
+import requests
+
+
+class Leanpub(requests.Session):
+    """Leanpub API Client object.
+
+    Based upon the requests.session object, so all underlying requests methods available.
+
+    Requires a Leanpub API Key to be instantiated.
+    """
+
+    def __init__(self, leanpub_api_key: str, **kwargs: dict) -> None:
+        """Instantiate a Leanpub client.
+
+        Args:
+            leanpub_api_key (str): A valid Leanpub API Key
+        """
+        super().__init__(**kwargs)
+        self.leanpub_api_key = leanpub_api_key
+        self.leanpub_url = "https://leanpub.com/"
+
+    def preview(self, book_slug: str) -> Union[Optional[requests.Response], Optional[requests.RequestException]]:
+        """Request a Preview be built of the book_slug provided.
+
+        Args:
+            book_slug (str): book_slug to generate a Preview of
+        """
+        url = f"{self.leanpub_url}{book_slug}/preview.json"
+        payload = {"api_key", self.leanpub_api_key}
+        try:
+            resp = self.post(url=url, payload=payload)
+        except requests.RequestException as exception:
+            return None, exception
+
+        return resp, None
