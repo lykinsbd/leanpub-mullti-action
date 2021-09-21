a4e19f4e05a279878c447b0e4c8a8278227aa856
Brett Lykins <lykinsbd@gmail.com>
2021-09-21 14:42:32 -0400

    add module level docstring

--- leanpub_multi_action/__init__.py
+++ leanpub_multi_action/__init__.py
@@ -1,5 +1,6 @@
-# Gather version information from project packaging
+"""Leanpub Multi Action is used to interact with the Leanpub.com API in GitHub Actions."""
 
+# Gather version information from project packaging
 try:
     from importlib import metadata
 except ImportError:
